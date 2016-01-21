e0c972979bb3147eef43e22f4097cfc11fa3a0f4
Pokechu22 <Pokechu022@gmail.com>
2016-01-21 07:47:31 -0800

    Rewrite to not be object-oriented.
    Javascript+OOP = bad idea.  The model instance actually wasn't in the
    collection anymore, so things got pretty messed up.
    
    A non-oop solution works a lot better.

--- search.js
+++ search.js
@@ -1,136 +1,153 @@
-var searchBox; //More wrong.
-
 searchTool = {
 	init: function() {
 		$('.search-form').each(function(idx, el) {
-			searchBox = new searchTool.SearchBox({el: el})
+			//TODO: 1 instance...
+			new searchTool.SearchBox({el: el})
 		})
 	}
 }
 
 searchTool.QueryTerm = Backbone.Model.extend({
-	field: "text",
-	
-	//Abstract?
-	canUseLucene: function() {
-		return true;
-	},
-	
-	getLuceneQuery: function() {
-		return "nyi"
-	},
-	
-	getCloudsearchQuery: function() {
-		return "nyi"
-	}
-});
-
-searchTool.BooleanQueryTerm = searchTool.QueryTerm.extend({
 	defaults: function() {
-		return {
-			value: false
-		}
+		var defaultField = "text";
+		
+		return _.extend({field : defaultField},
+				this.typeDefaults[this.termTypes[defaultField]]);
 	},
 	
-	canUseLucene: function() {
-		return true;
+	initialize: function() {
+		//Is this the right way?
+		this.on("change:field", this.recalculateType);
 	},
 	
-	getLuceneQuery: function() {
-		return this.get("field") + ":" + this.get("value");
-	},
-	
-	getCloudsearchQuery: function() {
-		return this.get("field") + ":" + (this.get("value") ? 1 : 0);
-	}
-});
-
-searchTool.TextualQueryTerm = searchTool.QueryTerm.extend({
-	defaults: function() {
-		return {
+	typeDefaults: {
+		// Keys are quoted because boolean is a keyword (even though it's unused)
+		"text": {
+			type: "text",
 			value: "",
 			isInverted: false,
 			requireAll: true,
-			isPhrase: false,
-		}
-	},
-	
-	canUseLucene: function() {
-		return this.get("isPhrase");
-	},
-	
-	getLuceneQuery: function() {
-		if (this.get("isPhrase")) {
-			return;
-		}
-		var words = this.get("value").split(" ")
-		var result = this.get("value");
-		if (!this.get("requireAll")) {
-			result = words.join(" OR ");
-		}
-		
-		if (words.length > 1) {
-			result = this.get("field") + ":(" + result + ")"
-		} else {
-			result = this.get("field") + ":" + result;
-		}
-		return result;
-	},
-	
-	getCloudsearchQuery: function() {
-		var result;
-		if (this.get("isPhrase")) {
-			//Note: Single quote followed by double quote means phrase query
-			result = "(field " + this.get("field") + " '\"" + this.get("value") + "\"')";
-		} else {
-			var words = this.get("value").split(" ");
-			// Not sure of the best way of doing this; right now I'll modify the array in-place
-			for (var i = 0; i < words.length; i++) {
-				words[i] = "(field " + this.get("field") + " '" + this.get("value") + "')"
-			}
-			result = words.join(" ");
-		}
-		
-		if (this.get("requireAll")) { // TODO: Check if there are multiple.
-			result = "(and " + result + ")";
-		} else {
-			result = "(or " + result + ")";
-		}
-		if (this.get("isInverted")) {
-			result = "(not " + result + ")";
-		}
-		return result;
-	}
-});
-
-searchTool.TimeQueryTerm = searchTool.QueryTerm.extend({
-	defaults: function() {
-		return {
-			field: "text", //Is this needed?
+			isPhrase: false
+		},
+		"boolean": {
+			type: "boolean",
+			value: false
+		},
+		"date": {
+			type: "date",
 			from: new Date(),
-			to: new Date(),
+			to: new Date()
+		}
+	},
+	
+	termTypes: {
+		text: 'text',
+		title: 'text',
+		selftext: 'text',
+		timestamp: 'date',
+		is_self: 'boolean',
+		author: 'text',
+		subreddit: 'text',
+		over18: 'boolean',
+		site: 'text',
+		url: 'text',
+		flair_text: 'text',
+		flair_css_class: 'text'
+	},
+	
+	recalculateType: function() {
+		var oldType = this.get("type");
+		var field = this.get("field");
+		var newType = this.termTypes[field];
+		
+		if (newType !== oldType) {
+			this.set(this.typeDefaults[newType]);
 		}
 	},
 	
 	canUseLucene: function() {
-		return false;
+		var type = this.get("type");
+		
+		if (type === "boolean") {
+			return true;
+		} else if (type === "text") {
+			return this.get("isPhrase");
+		} else if (type === "date") {
+			return false;
+		}
 	},
 	
 	getLuceneQuery: function() {
-		return "NA";
+		var type = this.get("type");
+		
+		if (type === "boolean") {
+			return this.get("field") + ":" + this.get("value");
+		} else if (type === "text") {
+			if (this.get("isPhrase")) {
+				return;
+			}
+			var words = this.get("value").split(" ")
+			var result = this.get("value");
+			if (!this.get("requireAll")) {
+				result = words.join(" OR ");
+			}
+			
+			if (words.length > 1) {
+				result = this.get("field") + ":(" + result + ")"
+			} else {
+				result = this.get("field") + ":" + result;
+			}
+			
+			if (this.get("isInverted")) {
+				return "NOT " + result;
+			} else {
+				return result;
+			}
+		} else if (type === "date") {
+			return "";
+		}
 	},
 	
 	getCloudsearchQuery: function() {
-		//TODO: Timezone stuff with cloudsearch - pretty sure it's not UTC.
+		var type = this.get("type");
 		
-		var fromTimestamp = this.get("from").getTime();
-		var toTimestamp = this.get("to").getTime();
-		
-		//Milliseconds to seconds - probably not the best way.
-		fromTimestamp = Math.round(fromTimestamp / 1000);
-		toTimestamp = Math.round(toTimestamp / 1000);
-		
-		return this.get("field") + ":" + fromTimestamp + ".." + toTimestamp;
+		if (type === "boolean") {
+			return this.get("field") + ":" + (this.get("value") ? 1 : 0);
+		} else if (type === "text") {
+			var result;
+			if (this.get("isPhrase")) {
+				//Note: Single quote followed by double quote means phrase query
+				result = "(field " + this.get("field") + " '\"" + this.get("value") + "\"')";
+			} else {
+				var words = this.get("value").split(" ");
+				// Not sure of the best way of doing this; right now I'll modify the array in-place
+				for (var i = 0; i < words.length; i++) {
+					words[i] = "(field " + this.get("field") + " '" + this.get("value") + "')"
+				}
+				result = words.join(" ");
+			}
+			
+			if (this.get("requireAll")) { // TODO: Check if there are multiple.
+				result = "(and " + result + ")";
+			} else {
+				result = "(or " + result + ")";
+			}
+			if (this.get("isInverted")) {
+				result = "(not " + result + ")";
+			}
+			return result;
+		} else if (type === "date") {
+			//TODO: Timezone stuff with cloudsearch - pretty sure it's not UTC.
+			
+			var fromTimestamp = this.get("from").getTime();
+			var toTimestamp = this.get("to").getTime();
+			
+			//Milliseconds to seconds - probably not the best way.
+			fromTimestamp = Math.round(fromTimestamp / 1000);
+			toTimestamp = Math.round(toTimestamp / 1000);
+			
+			return this.get("field") + ":" + fromTimestamp + ".." + toTimestamp;
+		}
 	}
 });
 
@@ -168,7 +185,7 @@ searchTool.SearchQuery = Backbone.Collection.extend({
 searchTool.QueryTermView = Backbone.View.extend({
 	tagName: "li",
 	
-	template: _.template("<select class=\"term-type\">" +
+	template: _.template("<select class=\"field-dropdown\">" +
 					"<option value=\"text\">Title and text</option>" +
 					"<option value=\"title\">Title</option>" +
 					"<option value=\"selftext\">Self text</option>" +
@@ -188,25 +205,9 @@ searchTool.QueryTermView = Backbone.View.extend({
 	textHTML: _.template("<select class=\"selectivity\" value=\"<%- selectivity %>\"><option value=\"all\">All of these words</option><option value=\"any\">Any of these words</option><option value=\"phrase\">All of these words in this order</option><option value=\"none\">None of these words</option></select><input class=\"text\" type=\"text\" value=\"<%- value %>\">"),
 	datepickerHTML: _.template("<label>From <input class=\"time-from\" type=\"text\"></label><label>To <input class=\"time-to\" type=\"text\"></label>"),
 	
-	// Can I directly reference the constructors?
-	termTypes: {
-		text: 'text',
-		title: 'text',
-		selftext: 'text',
-		timestamp: 'date',
-		is_self: 'boolean',
-		author: 'text',
-		subreddit: 'text',
-		over18: 'boolean',
-		site: 'text',
-		url: 'text',
-		flair_text: 'text',
-		flair_css_class: 'text'
-	},
-	
 	// Might be excessive
 	events: {
-		"change .term-type" : "termTypeChanged",
+		"change .field-dropdown" : "fieldDropdownChanged",
 		"click .boolean-toggle" : "booleanValueChanged",
 		"change .selectivity" : "selectivityChanged",
 		"input .text" : "textChanged"
@@ -215,62 +216,52 @@ searchTool.QueryTermView = Backbone.View.extend({
 	
 	initialize: function() {
 		this.$el.html(this.template()); //Does this really need to be a template anymore?  I assume it will be when I18n is factored in...
-	
-		this.listenTo(this.model, 'change', this.render);
+		
+		this.listenTo(this.model, 'change:type', this.termTypeChanged);
+		
+		//this.listenTo(this.model, 'change', this.render);
 		this.listenTo(this.model, 'destroy', this.remove);
-
-		this.termTypeChanged({target:this.$(".term-type")[0]}); //TODO: this is definitely the wrong way.
+		
+		// Should this be called?
+		this.termTypeChanged();
 	},
 	
-	termTypeChanged: function(e) {
-		var field = e.target.value;
+	fieldDropdownChanged: function(e) {
+		var newField = e.target.value;
 		
-		var oldType = this.termTypes[this.model.get("field")];
-		var newType = this.termTypes[field];
+		this.model.set("field", newField);
+	},
+	
+	termTypeChanged: function() {
+		var newType = this.model.get("type");
 		
-		if (oldType === newType) {
-			// Don't need to change type
-			this.model.set("field", field);
-		} else {
-			if (newType === "text") {
-				this.model = new searchTool.TextualQueryTerm({field: field});
-				
-				var selectivity;
-				
-				if (this.model.get("isInverted")) {
-					selectivity = "none";
-				} else if (this.model.get("isPhrase")) {
-					selectivity = "phrase";
-				} else if (!this.model.get("requireAll")) {
-					selectivity = "any";
-				} else {
-					selectivity = "all";
-				}
-				
-				this.$(".term-data").html(this.textHTML({value: this.model.get("value"), selectivity: selectivity}));
-			} else if (newType === "boolean") {
-				this.model = new searchTool.BooleanQueryTerm({field: field});
-				
-				this.$(".term-data").html(this.booleanHTML({checked: (this.model.get("value") ? "checked" : "")}));
-			} else if (newType === "date") {
-				this.model = new searchTool.TimeQueryTerm({field: field});
-				
-				//Will format wrong.
-				this.$(".term-data").html(this.datepickerHTML({from: this.model.get("from"), to: this.model.get("to")}));
+		if (newType === "text") {
+			var selectivity;
+			
+			if (this.model.get("isInverted")) {
+				selectivity = "none";
+			} else if (this.model.get("isPhrase")) {
+				selectivity = "phrase";
+			} else if (!this.model.get("requireAll")) {
+				selectivity = "any";
+			} else {
+				selectivity = "all";
 			}
 			
-			// Is this needed?
-			
-			this.listenTo(this.model, 'change', this.render);
-			this.listenTo(this.model, 'destroy', this.remove);
-			
-			// How about this?
-			this.render();
+			this.$(".term-data").html(this.textHTML({value: this.model.get("value"), selectivity: selectivity}));
+		} else if (newType === "boolean") {
+			this.$(".term-data").html(this.booleanHTML({checked: (this.model.get("value") ? "checked" : "")}));
+		} else if (newType === "date") {
+			//Will format wrong.
+			this.$(".term-data").html(this.datepickerHTML({from: this.model.get("from"), to: this.model.get("to")}));
 		}
+		
+		// I don't think this is needed...
+		this.render();
 	},
 	
 	booleanValueChanged: function(e) {
-		this.model.set("value", e.target.value);
+		this.model.set("value", e.target.checked);
 	},
 	selectivityChanged: function(e) {
 		var value = e.target.value;
@@ -296,9 +287,9 @@ searchTool.SearchBox = Backbone.View.extend({
 
 		this.listenTo(this.query, 'add', this.addOne);
 		this.listenTo(this.query, 'reset', this.addAll);
-		this.listenTo(this.query, 'changed', console.log);
+		this.listenTo(this.query, 'change', this.render);
 
-		this.query.add(new searchTool.TextualQueryTerm());
+		this.query.add(new searchTool.QueryTerm());
 		console.log(this.query);
 	},
 	
@@ -306,7 +297,7 @@ searchTool.SearchBox = Backbone.View.extend({
 		"click .add-search-option": "addOption"
 	},
 	addOption: function(e) {
-		this.query.add(new searchTool.TextualQueryTerm());
+		this.query.add(new searchTool.QueryTerm());
 	},
 	
 	render: function() {
